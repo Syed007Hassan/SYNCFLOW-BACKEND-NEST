bed78a8ea0197aa9c4da40b07ee4fdabbe80c64a
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-09-07 23:36:56 +0500

    orm config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { StripeModule } from './stripe/stripe.module';
+import { PostgreSqlDataSource } from './config/ormConfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `.env`,
     }),
+    PostgreSqlDataSource,
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    ProductModule,
     AuthModule,
     UserModule,
-    StripeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/ormConfig.ts
@@ -0,0 +1,20 @@
+import { configDotenv } from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+configDotenv();
+
+export const PostgreSqlDataSource = TypeOrmModule.forRootAsync({
+  name: 'postgres',
+  useFactory: async (config: ConfigService) => ({
+    type: 'postgres',
+    host: config.get('DB_HOST'),
+    port: config.get('DB_PORT'),
+    username: config.get('DB_USER'),
+    password: config.get('DB_PASSWORD'),
+    database: config.get('DB_NAME'),
+    synchronize: true,
+    logging: true,
+  }),
+  inject: [ConfigService],
+});
