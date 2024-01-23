4cdfbd9b0d363055719db6b5937edfba1bf1089c
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-01-23 23:55:04 +0500

    Commented out getPokemon method in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -253,21 +253,21 @@ export class AuthService {
     return payloadReturn;
   }
 
-  async getPokemon(id: number): Promise<string> {
-    // check if data is in cache:
-    const cachedData = await this.cacheService.get<{ name: string }>(
-      id.toString(),
-    );
-    if (cachedData) {
-      console.log(`Getting data from cache!`);
-      return `${cachedData.name}`;
-    }
+  // async getPokemon(id: number): Promise<string> {
+  //   // check if data is in cache:
+  //   const cachedData = await this.cacheService.get<{ name: string }>(
+  //     id.toString(),
+  //   );
+  //   if (cachedData) {
+  //     console.log(`Getting data from cache!`);
+  //     return `${cachedData.name}`;
+  //   }
 
-    // if not, call API and set the cache:
-    const { data } = await this.httpService.axiosRef.get(
-      `https://pokeapi.co/api/v2/pokemon/${id}`,
-    );
-    await this.cacheService.set(id.toString(), data);
-    return await `${data.name}`;
-  }
+  //   // if not, call API and set the cache:
+  //   const { data } = await this.httpService.axiosRef.get(
+  //     `https://pokeapi.co/api/v2/pokemon/${id}`,
+  //   );
+  //   await this.cacheService.set(id.toString(), data);
+  //   return await `${data.name}`;
+  // }
 }
