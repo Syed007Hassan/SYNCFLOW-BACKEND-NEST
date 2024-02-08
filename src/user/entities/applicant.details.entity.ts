a932401e4c8e4479863877ee8396872a36f5711f
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-02-08 20:28:25 +0500

    Interfaces for applicantLocation, education

--- src/user/entities/applicant.details.entity.ts
+++ src/user/entities/applicant.details.entity.ts
@@ -9,23 +9,36 @@ import {
   OneToOne,
 } from 'typeorm';
 import { Applicant } from './user.entity';
-
+import { Education } from '../dto/education.interface';
+import { ApplicantLocation } from '../dto/applicantLocation.interface';
 @Entity('applicantDetails')
 export class ApplicantDetails {
   @PrimaryGeneratedColumn()
   applicantDetailsId: number;
 
   @Column({ nullable: false })
+  dob: Date;
+
+  @Column({ nullable: false })
+  gender: string;
+
+  @Column({ nullable: false })
+  aboutMe: string;
+
+  @Column({ nullable: false, type: 'jsonb' })
+  education: Education[];
+
+  @Column({ nullable: false, type: 'jsonb' })
+  skills: string[];
+
+  @Column({ nullable: false, type: 'jsonb' })
+  location: ApplicantLocation;
+
+  @Column({ nullable: false })
   expertise: string;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, type: 'jsonb' })
   experience: string;
-
-  @Column({ nullable: false })
-  education: string;
-
-  @Column({ nullable: false })
-  skills: string;
 
   @Column({ nullable: false })
   languages: string;
