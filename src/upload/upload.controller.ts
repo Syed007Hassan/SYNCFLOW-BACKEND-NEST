625bf0c5b44e04cb2f6957ff133b77e7543e0186
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-02-12 22:49:48 +0500

    Add multer package and implement file upload functionality

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -7,7 +7,10 @@ import {
   Param,
   Delete,
   UploadedFile,
+  ParseFilePipe,
   UseInterceptors,
+  MaxFileSizeValidator,
+  FileTypeValidator,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { CreateUploadDto } from './dto/create-upload.dto';
@@ -20,7 +23,45 @@ import { FileInterceptor } from '@nestjs/platform-express';
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
-  @Post('profile')
+  @Post('profilePicture')
   @UseInterceptors(FileInterceptor('file'))
-  create(@UploadedFile() file: Express.Multer.File) {}
+  async profilePicture(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 10000 }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    try {
+      const response = await this.uploadService.uploadFile(file);
+      return { success: true, data: response };
+    } catch (e) {
+      return { success: false, message: e.message };
+    }
+  }
+
+  @Post('resume')
+  @UseInterceptors(FileInterceptor('file'))
+  async resume(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000000 }),
+          new FileTypeValidator({ fileType: '.(pdf|doc)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    try {
+      const response = await this.uploadService.uploadFile(file);
+      return { success: true, data: response };
+    } catch (e) {
+      return { success: false, message: e.message };
+    }
+  }
 }
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,26 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUploadDto } from './dto/create-upload.dto';
 import { UpdateUploadDto } from './dto/update-upload.dto';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class UploadService {
-  create(createUploadDto: CreateUploadDto) {
-    return 'This action adds a new upload';
+  private s3: AWS.S3;
+  private AWS_S3_BUCKET_NAME: string;
+
+  async onModuleInit() {
+    this.AWS_S3_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+    this.s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    });
   }
 
-  findAll() {
-    return `This action returns all upload`;
+  async uploadFile(file) {
+    const { originalname } = file;
+    console.log(this.AWS_S3_BUCKET_NAME);
+
+    return await this.s3_upload(
+      file.buffer,
+      this.AWS_S3_BUCKET_NAME,
+      originalname,
+      file.mimetype,
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} upload`;
-  }
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: process.env.AWS_S3_REGION,
+      },
+    };
 
-  update(id: number, updateUploadDto: UpdateUploadDto) {
-    return `This action updates a #${id} upload`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} upload`;
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+      return s3Response;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
