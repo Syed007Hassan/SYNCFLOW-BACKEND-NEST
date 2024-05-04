cc2970bf208d4eb0ae7742fcc696cdae62d55179
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-05-05 04:08:51 +0500

    chore: Update Redis configuration for cache module
    ```

--- src/config/OrmConfig.ts
+++ src/config/OrmConfig.ts
@@ -7,29 +7,32 @@ import { Company } from 'src/company/entities/company.entity';
 
 configDotenv();
 
-// export const PostgreSqlDataSource: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT),
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   schema: process.env.DB_SCHEMA,
-//   entities: [User, Employer],
-//   synchronize: true,
-//   logging: true,
-// };
-
+// For Azure Postgres
 export const PostgreSqlDataSource: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.PG_HOST,
-  port: parseInt(process.env.PG_PORT),
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DB,
+  host: process.env.PGHOST,
+  port: parseInt(process.env.PGPORT),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   schema: process.env.DB_SCHEMA,
-  // entities: [Applicant, Recruiter, Company],
   autoLoadEntities: true,
   synchronize: true,
   logging: true,
 };
+
+// For Docker Use
+// export const PostgreSqlDataSource: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: process.env.PG_HOST,
+//   port: parseInt(process.env.PG_PORT),
+//   username: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DB,
+//   schema: process.env.DB_SCHEMA,
+//   // entities: [Applicant, Recruiter, Company],
+//   autoLoadEntities: true,
+//   synchronize: true,
+//   logging: true,
+//   ssl: false,
+// };
